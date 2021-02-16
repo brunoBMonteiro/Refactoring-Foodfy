84dff1e047bffebaefb8677cf6d8933ba7cd16ce
brunoBMonteiro <brunobilheri@hotmail.com>
2021-02-15 21:34:31 -0300

    configurando template engine, nunjucks

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
+
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express:server
+})
 
 server.get("/", function(req, res){
     return res.send("Hi")
