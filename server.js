36ddee437665e56ab0e735bf6f32f9b90e494858
brunoBMonteiro <brunobilheri@hotmail.com>
2021-02-15 21:28:28 -0300

    importando arquivos do front,e intalando nunjucks, template engine

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const server = express()
 
 server.get("/", function(req, res){
-    return res.send("HI")
+    return res.send("Hi")
 })
 
 server.listen(5000, function(){
